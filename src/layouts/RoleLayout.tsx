a46d0220ff41f38510844212488d80d22ad6ab20
vsutariya-ontic <vsutariya@ontic.co>
2024-02-13 16:36:17 +0530

    First version added

---
+++ src/layouts/RoleLayout.tsx
@@ -0,0 +1,22 @@
+import { Badge, Box } from "@mui/material";
+import Topbar from "./Topbar";
+import { useTheme } from "@emotion/react";
+import Orders from "../features/shared/orders/Orders";
+import AuthLayout from "../features/shared/auth/AuthLayout";
+import { useAuthStore } from "../authstore/store";
+import { CartLayout } from "../features/employee/cart/CartLayout";
+import { MenuLayout } from "../features/shared/menu/MenuLayout";
+import { EmployeeLayout } from "./EmployeeLayout";
+import { PantryboyLayout } from "./PantryboyLayout";
+
+const BaseLayout = ({ path }: any) => {
+    const role = useAuthStore((state) => state.role);
+    return (
+        <>
+            {role === "employee" && <EmployeeLayout path={path} />}
+            {role === "pantryboy" && <PantryboyLayout path={path} />}
+        </>
+    );
+};
+
+export default BaseLayout;
